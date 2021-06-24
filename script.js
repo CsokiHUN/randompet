0039d37c1f397a71810d53d9549422cd2a75d661
CsokiHUN <feketetibi71@gmail.com>
2021-06-24 10:43:59 +0200

    add cat button :3

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const App = Vue.createApp({
     data() {
         return {
+            types: {
+                'dog': {
+                    url: 'https://dog.ceo/api/breeds/image/random',
+                    arg: 'message',
+                },
+                'cat': {
+                    url: 'https://api.thecatapi.com/v1/images/search',
+                    arg: 'url'
+                }
+            },
             imgSource: false
         }
     },
     methods: {
-        async getImage() {
+        async getImage(typ) {
+            if (!typ) {
+                typ = 'dog';
+            }
+            const typeData = this.types[typ];
+
             this.imgSource = false;
 
-            const response = await fetch('https://dog.ceo/api/breeds/image/random');
-            const json = await response.json();
+            const response = await fetch(typeData.url);
+            let json = await response.json();
 
-            if (json.status == 'success') {
-                this.imgSource = json.message;
+            if (json[0]) {
+                json = json[0];
             }
+
+            if (json) {
+                this.imgSource = json[typeData.arg];
+            }
+
         }
     },
 })
